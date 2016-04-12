a57920d55d9b460c11d6d79d14899f07964c8b4a
Jordan Sanders <jordan@sanders.tech>
2016-04-11 22:01:21 -0500

    Update sensor.py

--- Code/OS/home/pi/sensor.py
+++ Code/OS/home/pi/sensor.py
@@ -1,8 +1,8 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 from pymouse import pymouse
 import time
 
-pirPin = 16
+pirPin = 13
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pirPin, GPIO.IN)
 m = PyMouse()
@@ -12,6 +12,6 @@ res = m.screen_size()
 while True:
     value = GPIO.input(pirPin)
     if value = True:
-        m.move(res-100,res)
-        m.move(res,res)
+        m.move(res[0]-100,res[1])
+        m.move(res[0],res[1])
     time.sleep(10)
